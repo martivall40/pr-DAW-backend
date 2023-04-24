d1c99da76b63e722aedade9d5ee5db084f592753
martivall <marti.vall.40@lacetania.cat>
2023-04-24 21:42:38 +0200

    actualitzar moongose i arreglat crides

--- controllers/price.js
+++ controllers/price.js
@@ -5,18 +5,17 @@ const Price = require('../models/price')
 
 const controller = {
   getPrices: function (req, res) {
-    Price.find({}).exec((err, price) => {
-      console.log(price)
-      if (err) {
-        console.log(err)
-        return res.status(500).send({ message: "'error al retornar les dades" })
-      }
-
+    Price.find({}).then((price) => {
+      
       if (!price) return res.status(404).send({ message: 'El projecte no existeix' })
 
       return res.status(200).send({
         price
       })
+    }).catch((err) => {
+      console.error(err)
+      return res.status(500).send({ message: "'error al retornar les dadesa" })
+      
     })
   },
 }
